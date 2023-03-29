9c9f5438895b894592bd4392cb52dcdce6d19173
wz2k <87819279+wz2k@users.noreply.github.com>
2023-03-29 21:06:09 +0800

    Fix checkstyle

--- src/main/java/seedu/address/Main.java
+++ src/main/java/seedu/address/Main.java
@@ -19,6 +19,10 @@ import javafx.application.Application;
  * to be the entry point of the application, we avoid this issue.
  */
 public class Main {
+    /**
+     * asdasdasd
+     * @param args sdfdsfg.
+     */
     public static void main(String[] args) {
         System.out.println("hello");
         Application.launch(MainApp.class, args);
